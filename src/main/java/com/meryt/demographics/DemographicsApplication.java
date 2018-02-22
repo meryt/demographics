f2b6bb40597259fef380ff78f9ac32cc3e4326c2
meryt <meryt@meryt.com>
2018-02-22 13:25:26 -0800

    Initial commit

---
+++ src/main/java/com/meryt/demographics/DemographicsApplication.java
@@ -0,0 +1,12 @@
+package com.meryt.demographics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemographicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemographicsApplication.class, args);
+	}
+}
