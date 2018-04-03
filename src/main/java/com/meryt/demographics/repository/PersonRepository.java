167cdb102c038eb59b08e4a725999ec2e9dc81d7
meryt <meryt@meryt.com>
2018-04-03 15:30:37 -0700

    make pageable

--- src/main/java/com/meryt/demographics/repository/PersonRepository.java
+++ src/main/java/com/meryt/demographics/repository/PersonRepository.java
@@ -1,11 +1,11 @@
 package com.meryt.demographics.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.meryt.demographics.domain.person.Person;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
 }
