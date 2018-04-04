c5853805477be45e6b1f0ca34fbc0f2f847c2686
meryt <meryt@meryt.com>
2018-04-04 15:29:57 -0700

    switch to occupations

--- src/main/java/com/meryt/demographics/repository/OccupationRepository.java
+++ src/main/java/com/meryt/demographics/repository/OccupationRepository.java
@@ -1,10 +1,11 @@
 package com.meryt.demographics.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.meryt.demographics.domain.Occupation;
 
 @Repository
-public interface OccupationRepository extends CrudRepository<Occupation, Long> {
+public interface OccupationRepository extends PagingAndSortingRepository<Occupation, Long> {
+
 }
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,56 +1,156 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import client from './client'
+import follow from './follow'
+
+const root = '/data';
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { persons: [] };
+        this.state = { occupations: [], attributes: [], pageSize: 20, links: {} };
     }
 
     componentDidMount() {
-        client({ method: 'GET', path: '/data/persons' })
-            .then(response => {
-                this.setState( { persons: response.entity._embedded.persons } );
-            });
+        this.loadFromServer(this.state.pageSize);
+    }
+
+    loadFromServer(pageSize) {
+        follow(client, root, [{rel: 'occupations', params: {size: pageSize}}])
+            .then(occupationCollection => {
+                return client({
+                    method: 'GET',
+                    path: occupationCollection.entity._links.profile.href,
+                    headers: {'Accept': 'application/schema+json'}
+                }).then(schema => {
+                    this.schema = schema.entity;
+                    return occupationCollection;
+                })
+            }).then(occupationCollection => {
+                this.setState({
+                    occupations: occupationCollection.entity._embedded.occupations,
+                    attributes: Object.keys(this.schema.properties),
+                    pageSize: pageSize,
+                    links: occupationCollection.entity._links});
+                });
+    }
+
+    onCreate(newOccupation) {
+        follow(client, root, ['occupations']).then(occupationCollection => {
+            return client({
+                method: 'POST',
+                path: occupationCollection.entity._links.self.href,
+                entity: newOccupation,
+                headers: { 'Content-Type': 'application/json' }
+            })
+        }).then(response => {
+            return follow(client, root, [
+                { rel: 'occupations', params: { 'size' : this.state.pageSize } }
+            ]);
+        }).then(response => {
+            if (typeof response.entity._links.last !== 'undefined') {
+                this.onNavigate(response.entity._links.last.href);
+            } else {
+                this.onNavigate(response.entity._links.self.href);
+            }
+        });
     }
 
     render() {
         return (
-            <PersonList persons={this.state.persons} />
+            <div>
+                <CreateOccupationDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+                <OccupationList occupations={this.state.occupations} />
+            </div>
         )
     }
-
 }
 
-class PersonList extends React.Component {
+class OccupationList extends React.Component {
     render() {
-        const persons = this.props.persons.map(person =>
-            <Person key={person._links.self.href} person={person} />
+        const occupations = this.props.occupations.map(occupation =>
+            <Occupation key={occupation._links.self.href} occupation={occupation} />
         );
 
         return (
             <table>
                 <tbody>
                     <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
+                        <th>Name</th>
+                        <th>Support Factor</th>
+                        <th>Male</th>
+                        <th>Female</th>
+                        <th>Rural</th>
                     </tr>
-                    {persons}
+                    {occupations}
                 </tbody>
             </table>
         )
     }
 }
 
-class Person extends React.Component {
+class Occupation extends React.Component {
     render() {
         return (
             <tr>
-                <td>{this.props.person.firstName}</td>
-                <td>{this.props.person.lastName}</td>
+                <td>{this.props.occupation.name}</td>
+                <td>{this.props.occupation.supportFactor}</td>
+                <td>{this.props.occupation.allowMale ? 'X' : '' }</td>
+                <td>{this.props.occupation.allowFemale ? 'X' : ''}</td>
+                <td>{this.props.occupation.rural ? 'X' : ''}</td>
             </tr>
+        )
+    }
+}
+
+class CreateOccupationDialog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let newOccupation = {};
+        this.props.attributes.forEach(attribute => {
+            newOccupation[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newOccupation);
+
+        // clear out dialog's inputs
+        this.props.attributes.forEach(attribute => {
+            ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        // Navigate away from the dialog to hide it
+        window.location = '#';
+    }
+
+    render() {
+        const inputs = this.props.attributes.map(attribute =>
+            <p key={attribute}>
+                <input type="text" placeholder={attribute} ref={attribute} className={"field"} />
+            </p>
+        );
+
+        return (
+            <div>
+                <a href="#createOccupation">Create</a>
+
+                <div id="createOccupation" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+
+                        <h2>Create new occupation</h2>
+
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
