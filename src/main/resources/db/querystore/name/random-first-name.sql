2decc1ddeea80467619de0b8fa477ed20dbd6e16
meryt <meryt@meryt.com>
2018-04-09 15:30:08 -0700

    names

--- src/main/resources/db/querystore/name/random-first-name.sql
+++ src/main/resources/db/querystore/name/random-first-name.sql
@@ -1,27 +1,33 @@
-/* WITH z AS (
-  SELECT
-    CASE WHEN (:onDate::DATE IS NOT NULL AND EXISTS (
-        SELECT from_date FROM names_first WHERE gender = :gender AND DATERANGE(from_date, to_date, '[)') @> :onDate::DATE)) THEN :onDate::DATE
-      WHEN :onDate::DATE IS NOT NULL AND (SELECT MIN(from_date) FROM names_first WHERE gender = :gender) > :onDate::DATE)
-        THEN (SELECT MIN(from_date) FROM names_first WHERE gender = :gender)
-      ELSE                                          ELSE
-        SELECT MIN(from_date) FROM names_first WHERE gender = :gender AND from_date <
-    MIN(from_date)
-  FROM names_first WHERE gender = :gender
-  AND (:onDate::DATE IS NOT NULL AND DATERANGE(from_date, to_date, '[)') @> :onDate::DATE)
-), */
 WITH
-  y AS (
-    SELECT
-      name,
-      weight,
-      SUM(weight) OVER (ORDER BY name) AS cum_weight
+min_from_date AS (
+   SELECT MIN(from_date) AS from_date
+   FROM names_first
+   WHERE gender = :gender
+),
+next_lowest_from_date AS (
+    SELECT MAX(from_date) AS from_date
     FROM names_first
     WHERE gender = :gender
-    AND (:onDate::DATE IS NULL OR DATERANGE(from_date, to_date, '[)') @> :onDate::DATE )
-    ORDER BY name
+    AND :onDate::DATE IS NOT NULL AND from_date < :onDate::DATE
 ),
-    rand AS (
+containing_from_date AS (
+     SELECT :onDate::DATE AS from_date
+     FROM names_first
+     WHERE gender = :gender
+     AND :onDate::DATE IS NOT NULL AND DATERANGE(from_date, to_date, '[)') @> :onDate::DATE
+     LIMIT 1
+),
+y AS (
+    SELECT
+      nf.name,
+      nf.weight,
+      SUM(nf.weight) OVER (ORDER BY nf.name) AS cum_weight
+    FROM names_first nf, min_from_date, next_lowest_from_date, containing_from_date
+    WHERE nf.gender = :gender
+    AND (DATERANGE(nf.from_date, nf.to_date, '[)') @> COALESCE(containing_from_date.from_date, next_lowest_from_date.from_date, min_from_date.from_date))
+    ORDER BY nf.name
+),
+rand AS (
       SELECT
         SUM(weight) AS total_weight,
         random()*(SELECT SUM(weight) FROM y) AS randval
